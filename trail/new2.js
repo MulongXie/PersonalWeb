4d4a5d7bc622f627612ce3426c05e27c7e7a5388
MulongXie <dsh15325@163.com>
2019-01-16 14:42:33 +0800

    v3 with fadein

--- trail/new2.js
+++ trail/new2.js
@@ -1,6 +1,12 @@
 
-function f(a, b) {
-    console.log(a + b);
+var i = 0;
+function f() {
+    console.log(i);
+    i++;
+    if (i < 10){
+        setTimeout(f, 500);
+    }
+
 }
 
-setTimeout(f, 1000, "ri", "sss")
+f();
