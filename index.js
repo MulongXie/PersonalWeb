c31332ba82a7d9a6ce5af32d5faa08b188342d24
MulongXie <dsh15325@163.com>
2022-07-01 20:55:55 +1000

    toggle content and side nav

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
-$(document).ready(function (){
-    $(window).scroll(function (){
-        let sideNav = $('#side-nav');
-        let sideContent = $('#side-content')
-        let sideContentDistTop = sideContent.offset().top - $(window).scrollTop();
-        if (sideContentDistTop <= 0){
-            sideNav.addClass('fixed-top');
-            sideNav.addClass('side-nav-fixed-top');
+var closeSideNav = false
+
+$(document).ready(function () {
+    toggleNavAndContent();
+
+    // sideNavCollapse();
+    $(window).scroll(function () {
+        sideNavOnScroll();
+    });
+
+    $('.collapse-btn').click(function (){
+        // sideNavCollapse();
+        closeSideNav = true
+        toggleNavAndContent(true);
+    })
+});
+
+function sideNavOnScroll(){
+    let sideNav = $('#side-nav');
+    let sideContent = $('#side-content');
+    let sideContentDistTop = sideContent.offset().top - $(window).scrollTop();
+
+
+    if(0 < sideContentDistTop && sideContentDistTop < $(window).height() * 0.25 && ! closeSideNav && ! sideNav.is(':visible')){
+        toggleNavAndContent();
+    }
+
+    // fix the side navbar on the top
+    if (sideContentDistTop <= 0){
+        sideNav.addClass('fixed-top');
+        sideNav.addClass('side-nav-fixed-top');
+        if (sideNav.is(':visible')){
             sideContent.addClass('offset-3')
         }
-        else{
-            sideNav.removeClass('fixed-top');
-            sideNav.removeClass('side-nav-fixed-top');
+    }
+    else{
+        sideNav.removeClass('fixed-top');
+        sideNav.removeClass('side-nav-fixed-top');
+        if (sideNav.is(':visible')){
             sideContent.removeClass('offset-3')
         }
-        console.log(sideContentDistTop);
-    });
-});
+    }
+}
+
+function toggleContent(){
+    console.log('content')
+    let sideContent = $('#side-content');
+    sideContent.toggleClass('col-9');
+    sideContent.toggleClass('col-12');
+}
+
+function toggleNav(){
+    console.log('nav', closeSideNav)
+    let sideNav = $('#side-nav');
+    sideNav.toggle('col-3')
+}
+
+function toggleNavAndContent(delayContent=false){
+    toggleNav();
+    if (delayContent){
+        setTimeout(toggleContent, 500);
+    }
+    else{
+        toggleContent();
+    }
+}
